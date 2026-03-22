4e30be7d97931c740540d29512c39dad3e9114a3
mannubaveja007 <23bis70088@cuchd.in>
2026-03-22 12:10:17 +0530

    Add: Redis Connection Pooling

--- redis_cli_client/src/main.rs
+++ redis_cli_client/src/main.rs
@@ -1,30 +1,77 @@
-use redis::Commands;
+// use redis::Commands;
+// use tokio::time::{sleep, Duration};
+
+
+// #[tokio::main]
+// async fn main() -> redis::RedisResult<()> {
+//     println!("Connecting to redis...");
+
+//     // 1. conencting to redis
+//     let client  = redis::Client::open("redis://default:jlSSAuQWiDzAQeTU4YZ29GVurjB1Sc0J@redis-10320.crce220.us-east-1-4.ec2.cloud.redislabs.com:10320")?;
+
+//     let mut con = client.get_connection()?; // connects to the client
+//     sleep(Duration::from_secs(2)).await;
+//     // 2. Set key - value pair
+//     println!("Setting key 'test'....");
+//     let _: () = con.set("test","Hellooooo Panchooooooo! Hogeyaaa oyeee Rust me")?;
+
+//     // 3. set the value back
+//     let result: String = con.get("test")?;
+//     let result2: String = con.get("test:v2")?;
+//     println!("Retrieved value: {}",result);
+//     println!("Retrieved value: {}",result2);
+
+//     // 4. Perform an integer operation
+//     let _: () = con.set("counter",10)?;
+//     let new_counter : i32 = con.incr("counter",0)?;
+//     println!("Incremented counter: {}", new_counter);
+
+//     Ok(())
+// }
+
+// connection Pooling and things
+
+use redis::AsyncCommands;
 use tokio::time::{sleep, Duration};
+use deadpool_redis::{Config,Runtime};
 
 
 #[tokio::main]
-async fn main() -> redis::RedisResult<()> {
-    println!("Connecting to redis...");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Connecting to redis pool...");
 
-    // 1. conencting to redis
-    let client  = redis::Client::open("redis://default:jlSSAuQWiDzAQeTU4YZ29GVurjB1Sc0J@redis-10320.crce220.us-east-1-4.ec2.cloud.redislabs.com:10320")?;
+    // 1. Configure the pool
+    
+    let redis_url  = "redis://default:jlSSAuQWiDzAQeTU4YZ29GVurjB1Sc0J@redis-10320.crce220.us-east-1-4.ec2.cloud.redislabs.com:10320";
 
-    let mut con = client.get_connection()?; // connects to the client
+    let cfg = Config::from_url(redis_url);
+
+
+    // 2. Create the pool(holds the hourses of connection)
+
+    let pool = cfg.create_pool(Some(Runtime::Tokio1))?;
+
+
+    let mut con = pool.get().await?; // connects to the client
+
     sleep(Duration::from_secs(2)).await;
-    // 2. Set key - value pair
+
+
+    // 3. Set key - value pair
     println!("Setting key 'test'....");
-    let _: () = con.set("test","Hellooooo Panchooooooo! Hogeyaaa oyeee Rust me")?;
+    let _: () = con.set("test","Hellooooo Panchooooooo! Hogeyaaa oyeee Rust me").await?;
 
     // 3. set the value back
-    let result: String = con.get("test")?;
-    let result2: String = con.get("test:v2")?;
+    let result: String = con.get("test").await?;
+    let result2: String = con.get("test:v2").await?;
     println!("Retrieved value: {}",result);
     println!("Retrieved value: {}",result2);
 
     // 4. Perform an integer operation
-    let _: () = con.set("counter",10)?;
-    let new_counter : i32 = con.incr("counter",11)?;
+    let _: () = con.set("counter",10).await?;
+    let new_counter : i32 = con.incr("counter",0).await?;
     println!("Incremented counter: {}", new_counter);
 
     Ok(())
 }
+
