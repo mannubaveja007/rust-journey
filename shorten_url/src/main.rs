bc99cd95f897b1af60d05f4e90a68156f9afe91a
mannubaveja007 <23bis70088@cuchd.in>
2026-04-04 13:55:48 +0530

    Add: shorten_url project #2

--- shorten_url/src/main.rs
+++ shorten_url/src/main.rs
@@ -6,14 +6,44 @@ use axum::{
     routing::{get, post, put},
 };
 
+use redis;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-
 use uuid::Uuid;
 // req / res structs
 //
 
-fn main() {
-    println!("Hello, world!");
+async fn some_handler(State(state): State<AppState>) -> impl IntoResponse {
+    let mut conn = state.redis.lock().unwrap();
+}
+
+#[derive(Clone)]
+struct AppState {
+    redis: Arc<Mutex<redis::Connection>>,
+}
+
+#[tokio::main]
+async fn main() {
+    // 1. Connect to redis
+    let client = redis::Client::open("redis_url").unwrap();
+    let mut conn = client.get_connection().unwrap();
+
+    // 2. Wrap in Arc<Mutext>> and put into the AppState
+    //
+    let state = AppState {
+        redis: Arc::new(Mutex::new(conn)),
+    };
+
+    // 3. Pass to router
+    //
+    let app = Router::new()
+        .route("/", get(some_handler))
+        .with_state(state);
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("Server running on http://127.0.0.1:3000");
+    axum::serve(listener, app).await.unwrap();
+    // println!("Hello, world!");
 }
