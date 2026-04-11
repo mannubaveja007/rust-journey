33a3961027b23d3ab0d6a2e91073baed68a98359
mannubaveja007 <23bis70088@cuchd.in>
2026-04-11 11:49:02 +0530

    Add: Take file input with all CLI feats/Compressor

--- Compressor/src/main.rs
+++ Compressor/src/main.rs
@@ -1,23 +1,53 @@
 // First take 2 number from argument and then add sum of them simp shit to move forward in order to learn  args and it's working
 //
-use clap::Parser;
+// use clap::Parser;
 
-/// Simple program to Take input of two number to perform some operation
-#[derive(Parser, Debug)]
+// /// Simple program to Take input of two number to perform some operation
+// #[derive(Parser, Debug)]
+// #[command(version, about, long_about = None)]
+// struct Args {
+//     /// Number 1
+//     #[arg(short = '1', long)]
+//     num1: u8,
+
+//     /// Number 2
+//     #[arg(short = '2', long)]
+//     num2: u8,
+// }
+
+// fn main() {
+//     let args = Args::parse();
+
+//     let result = args.num1 + args.num2;
+//     println!("Your Result : {result}");
+// }
+
+// input an file to compress and --out flag to output the file which is compreseed
+//
+// example: ./compressor --in <file> --out <output_file>
+
+use clap::Parser as _;
+use clap_file::{Input, Output};
+use std::io::{self, BufRead as _, Write as _};
+/// Simple program to Take input an file to compress and output an compressed file
+#[derive(clap::Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Number 1
-    #[arg(short = '1', long)]
-    num1: u8,
+    /// Input of File
+    #[arg(short = 'f', long)]
+    file: Input,
 
-    /// Number 2
-    #[arg(short = '2', long)]
-    num2: u8,
+    /// Output of File
+    #[arg(short = 'o', long)]
+    out: Output,
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = Args::parse();
+    let input = args.file.lock();
+    let mut output = args.out.lock();
+    // let result = args.num1 + args.num2;
+    // println!("Your Result : {result}");
+    Ok(())
 
-    let result = args.num1 + args.num2;
-    println!("Your Result : {result}");
 }
