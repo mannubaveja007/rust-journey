a43135aa69a60be32c388c71b8cd6371481a7626
mannubaveja007 <23bis70088@cuchd.in>
2026-04-13 09:53:08 +0530

    Add: CSV file

--- learn_csv_read/src/main.rs
+++ learn_csv_read/src/main.rs
@@ -2,10 +2,24 @@ use std::{error::Error, fs::File};
 
 use csv::Reader;
 
+fn read_csv(path: &str) -> Result<Reader<File>, Box<dyn Error>> {
+    let file = File::open(path)?;
+    Ok(Reader::from_reader(file))
+}
+
+fn write_csv(path: &str, records: &[Vec<String>]) -> Result<(), Box<dyn Error>> {
+    let mut csv = csv::Writer::from_path(path)?;
+
+    for record in records {
+        csv.write_record(record)?;
+    }
+    csv.flush()?;
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Printing the information read from CSV file!");
-    let file = File::open("Morgan Stanly.csv")
-        .expect("Dear check your CSV file");
+    let file = File::open("Morgan Stanly.csv").expect("Dear check your CSV file");
     let mut csv = csv::Reader::from_reader(file);
     for result in csv.records() {
         let record = result?;
