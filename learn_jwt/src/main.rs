261ee3c834687304da9a4c2b0e50496b046c68b0
mannubaveja007 <23bis70088@cuchd.in>
2026-04-13 16:42:02 +0530

    Add: JWT test done + Game On

--- learn_jwt/src/main.rs
+++ learn_jwt/src/main.rs
@@ -1,7 +1,8 @@
 // new Cool thing!
+use jsonwebtoken::{DecodingKey, EncodingKey};
 use serde::{Deserialize, Serialize};
-
 // todo -> Generate JWT and explore it's library from docs.rs
+use jsonwebtoken::{Algorithm, Header, encode};
 
 #[derive(Serialize, Deserialize)]
 struct Claims {
@@ -12,4 +13,17 @@ struct Claims {
 
 fn main() {
     println!("Hello, world!");
+    let val: Claims = Claims {
+        sub: "bruhh".to_string(),
+        email: "your_email@idk.io".to_string(),
+        exp: 23476238432,
+    };
+    let token = encode(
+        &Header::default(),
+        &val,
+        &EncodingKey::from_secret("Nhi_Bataungaa".as_ref()),
+    )
+    .expect("unable to generate token");
+
+    println!("token => {}", token);
 }
