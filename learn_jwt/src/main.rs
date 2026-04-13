8bcfb0bf14f47e918fc171de285c1cb05e6da048
mannubaveja007 <23bis70088@cuchd.in>
2026-04-13 16:32:42 +0530

    Add: Auth=> JWT Project

--- learn_jwt/src/main.rs
+++ learn_jwt/src/main.rs
@@ -1,4 +1,15 @@
 // new Cool thing!
+use serde::{Deserialize, Serialize};
+
+// todo -> Generate JWT and explore it's library from docs.rs
+
+#[derive(Serialize, Deserialize)]
+struct Claims {
+    sub: String, // User ID
+    email: String,
+    exp: usize, // Expiration timestamp
+}
+
 fn main() {
     println!("Hello, world!");
 }
