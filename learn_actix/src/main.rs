2881775d34157aafa3c16303f696e76f138bd8e3
mannubaveja007 <23bis70088@cuchd.in>
2026-04-19 13:26:48 +0530

    Add : hello from Actix

--- learn_actix/src/main.rs
+++ learn_actix/src/main.rs
@@ -1,5 +1,10 @@
 use actix_web::{get,post,web,App,HttpResponse,HttpServer,Responder};
 
+
+#[get("/")]
+async fn hello() -> impl  Responder {
+    HttpResponse::Ok().body("Kehh haal haiii 👀")
+}
 fn main() {
     println!("Hello, world!");
 }
