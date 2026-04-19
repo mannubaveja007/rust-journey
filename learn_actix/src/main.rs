66aa5d77b7e603b74b5630353e34ef1f0980352b
mannubaveja007 <23bis70088@cuchd.in>
2026-04-19 13:28:02 +0530

    Add : Echo handler [Actix]

--- learn_actix/src/main.rs
+++ learn_actix/src/main.rs
@@ -5,6 +5,11 @@ use actix_web::{get,post,web,App,HttpResponse,HttpServer,Responder};
 async fn hello() -> impl  Responder {
     HttpResponse::Ok().body("Kehh haal haiii 👀")
 }
+
+#[post("/echo")]
+async fn echo(request_string : String) -> impl  Responder {
+    HttpResponse::Ok().body(request_string)
+}
 fn main() {
     println!("Hello, world!");
 }
