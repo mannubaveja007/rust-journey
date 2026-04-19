2c47dc5ad3f287c9e52fdcfccde8757e26581056
mannubaveja007 <23bis70088@cuchd.in>
2026-04-19 13:35:54 +0530

    Add : Actix HTTP Server

--- learn_actix/src/main.rs
+++ learn_actix/src/main.rs
@@ -1,15 +1,27 @@
-use actix_web::{get,post,web,App,HttpResponse,HttpServer,Responder};
-
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 
 #[get("/")]
-async fn hello() -> impl  Responder {
+async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Kehh haal haiii 👀")
 }
 
 #[post("/echo")]
-async fn echo(request_string : String) -> impl  Responder {
+async fn echo(request_string: String) -> impl Responder {
     HttpResponse::Ok().body(request_string)
 }
-fn main() {
-    println!("Hello, world!");
+async fn manual() -> impl Responder {
+    HttpResponse::Ok().body("Hello, World from Actix🦀")
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Welcome to the Actix Server!");
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
