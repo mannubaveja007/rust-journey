dbd5a34f7534bb64797455ed07e78bebf137cc81
mannubaveja007 <23bis70088@cuchd.in>
2026-04-21 00:34:21 +0530

    feat: imports added

--- learn_axum_jwt_api/src/main.rs
+++ learn_axum_jwt_api/src/main.rs
@@ -1,3 +1,18 @@
+use serde::{Serialize,Deserialize};
+use jsonwebtoken::{encode, EncodingKey, Header};
+use chrono::Utc;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,   // User ID
+    email: String,
+    exp: usize,   // Expiration timestamp
+}
+
+
+
+
+
 fn main() {
     println!("Now We are going to start Axum API endpoint + JWT proper Scalable API!");
 }
