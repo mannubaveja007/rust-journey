cf9dfc488ccfbb4323fe633cd16192a04d134e33
mannubaveja007 <23bis70088@cuchd.in>
2026-04-21 23:37:45 +0530

    feat: imports added

--- spotify_api/src/main.rs
+++ spotify_api/src/main.rs
@@ -1,5 +1,11 @@
 use reqwest;
+use reqwest::header::{AUTHORIZATION,USER_AGENT,ACCEPT,CONTENT_TYPE};
+use serde::{Deserialize,Serialize};
 
-fn main() {
-    println!("This is an project which will be using spotify API and it will be acting as an search system for spotify using their API!");
+
+#[tokio::main]
+async fn main() {
+    println!(
+        "This is an project which will be using spotify API and it will be acting as an search system for spotify using their API!"
+    );
 }
