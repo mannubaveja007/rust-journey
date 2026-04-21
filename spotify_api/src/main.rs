8d3a9a185df9affdab8cfa0233d4fbce7acd8cb5
mannubaveja007 <23bis70088@cuchd.in>
2026-04-21 23:45:27 +0530

    feat: structs defined

--- spotify_api/src/main.rs
+++ spotify_api/src/main.rs
@@ -1,7 +1,47 @@
 use reqwest;
-use reqwest::header::{AUTHORIZATION,USER_AGENT,ACCEPT,CONTENT_TYPE};
-use serde::{Deserialize,Serialize};
+use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE, USER_AGENT};
+use serde::{Deserialize, Serialize};
 use std::env;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExternalUrls {
+    spotify: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Artist {
+    name: String,
+    external_link: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Album {
+    name : String,
+    artists : Vec<Artist>,
+    external_urls : ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Track {
+    name : String,
+    href : String,
+    popularity : u32,
+    album : Album,
+    external_urls : ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct APIResponse {
+    tracks : Items<Track>,
+    
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Items<T> {
+    items : Vec<T>
+}
+
+
 
 #[tokio::main]
 async fn main() {
