b83dfa8a23cf0b2a4810347f0aa8f0e50c0a2c44
mannubaveja007 <23bis70088@cuchd.in>
2026-04-24 10:43:32 +0530

    feat : Hello World endpoint/Axum + Tokio + MongoDB

--- axum_mongodb/src/main.rs
+++ axum_mongodb/src/main.rs
@@ -1,5 +1,25 @@
-fn main() {
+use axum::{
+    Router,
+    extract::{Path, State},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, post},
+};
+use tokio;
+
+async fn plain_text() -> &'static str {
+    "Hello World endpoint!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(plain_text));
     println!(
         "In this we are going to use axum and build and simple todolist and connect it with mongodb. Looks Good!!"
     );
+    let listner = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("Server is running on http://127.0.0.1:3000");
+    axum::serve(listner, app).await.unwrap();
 }
