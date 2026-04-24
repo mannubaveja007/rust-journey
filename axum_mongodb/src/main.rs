3e1e2b5a72f0f2f6a8ae0bdc985dd2ec52ced1cd
mannubaveja007 <23bis70088@cuchd.in>
2026-04-24 11:46:09 +0530

    feat : Echo Response. endpoint/Axum + Tokio + MongoDB

--- axum_mongodb/src/main.rs
+++ axum_mongodb/src/main.rs
@@ -1,25 +1,43 @@
 use axum::{
-    Router,
+    Json, Router,
     extract::{Path, State},
     http::StatusCode,
     response::{IntoResponse, Response},
     routing::{get, post},
 };
+use serde::{Deserialize, Serialize};
 use tokio;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct GetDataResponse {
+    text: String,
+}
 
 async fn plain_text() -> &'static str {
     "Hello World endpoint!"
 }
 
+async fn echo_response(
+    Path(text): Path<String>,
+    Json(body): Json<GetDataResponse>,
+) -> impl IntoResponse {
+    Json(GetDataResponse {
+        text: format!("{} ", body.text),
+    })
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(plain_text));
+    let app = Router::new()
+        .route("/", get(plain_text))
+        .route("/echo/{text}", post(echo_response));
+
     println!(
         "In this we are going to use axum and build and simple todolist and connect it with mongodb. Looks Good!!"
     );
-    let listner = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
     println!("Server is running on http://127.0.0.1:3000");
-    axum::serve(listner, app).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
