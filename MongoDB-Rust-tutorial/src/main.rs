5fe05f60007a9d46d50e8dedcd458e93dfeffe34
mannubaveja007 <23bis70088@cuchd.in>
2026-04-26 12:33:07 +0530

    feat : MongoDB tutorial/ Axum API

--- MongoDB-Rust-tutorial/src/main.rs
+++ MongoDB-Rust-tutorial/src/main.rs
@@ -1,19 +1,40 @@
+use axum::{Json, Router, body::Body, routing::get,extract::State};
 use mongodb::{
     Client, Collection,
     bson::{Document, doc},
 };
 use tokio;
+use std::sync::Arc;
+use tokio::net::TcpListener;
+
+
+#[derive(Clone)]
+struct AppState{
+    client : Client,
+}
+
+async fn get_stocks(State(state) : State<Arc<AppState>>) -> Result<Json<Document>, String> {
+    let db = state.client.database("stockDB");
+    let my_coll: Collection<Document> = db.collection("stocks");
+
+    match my_coll.find_one(doc! {"name": "Tesla"}).await {
+            Ok(Some(doc)) => Ok(Json(doc)),
+            Ok(None) => Err("Stock not found".into()),
+            Err(e) => Err(e.to_string()),
+        }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), mongodb::error::Error> {
     println!("Connecting to the database...");
     let client = Client::with_uri_str("mongodb://localhost:27017/").await?;
 
-    let db = client.database("stockDB");
+    let state = Arc::new(AppState { client});
+    let app = Router::new()
+        .route("/", get(get_stocks))
+        .with_state(state);
 
-    let my_coll: Collection<Document> = db.collection("stocks");
-
-    let my_stocks = my_coll.find_one(doc! {"name" : "Tesla" }).await?;
-    println!("My stock : \n{:#?} ", my_stocks);
+    let listner = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listner, app).await.unwrap();
     Ok(())
 }
