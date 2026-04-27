3a5e33ef943d0fb2ef02278603f351a5e2e16715
mannubaveja007 <23bis70088@cuchd.in>
2026-04-27 23:30:41 +0530

    feat : added structs

--- MongoDB-Rust-tutorial/src/main.rs
+++ MongoDB-Rust-tutorial/src/main.rs
@@ -1,38 +1,44 @@
-use axum::{Json, Router, body::Body, routing::get,extract::State};
+use axum::{Json, Router, body::Body, extract::State, routing::get};
 use mongodb::{
     Client, Collection,
     bson::{Document, doc},
 };
+use serde::{Deserialize, Serialize};
+use std::{sync::Arc};
 use tokio;
-use std::sync::Arc;
 use tokio::net::TcpListener;
 
-
-#[derive(Clone)]
-struct AppState{
-    client : Client,
+#[derive(Debug, Deserialize, Serialize)]
+struct StockUsers {
+    #[serde(rename = "_id")]
+    id: u32,
+    walletAddress: String,
 }
 
-async fn get_stocks(State(state) : State<Arc<AppState>>) -> Result<Json<Document>, String> {
+#[derive(Clone)]
+struct AppState {
+    client: Client,
+}
+
+async fn get_stocks(State(state): State<Arc<AppState>>) -> Result<Json<Document>, String> {
     let db = state.client.database("stockDB");
     let my_coll: Collection<Document> = db.collection("stocks");
 
     match my_coll.find_one(doc! {"name": "Tesla"}).await {
-            Ok(Some(doc)) => Ok(Json(doc)),
-            Ok(None) => Err("Stock not found".into()),
-            Err(e) => Err(e.to_string()),
-        }
+        Ok(Some(doc)) => Ok(Json(doc)),
+        Ok(None) => Err("Stock not found".into()),
+        Err(e) => Err(e.to_string()),
+    }
 }
+
 
 #[tokio::main]
 async fn main() -> Result<(), mongodb::error::Error> {
     println!("Connecting to the database...");
     let client = Client::with_uri_str("mongodb://localhost:27017/").await?;
 
-    let state = Arc::new(AppState { client});
-    let app = Router::new()
-        .route("/", get(get_stocks))
-        .with_state(state);
+    let state = Arc::new(AppState { client });
+    let app = Router::new().route("/", get(get_stocks)).with_state(state);
     println!("Backend in running on 127.0.0.1:3000");
     let listner = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listner, app).await.unwrap();
